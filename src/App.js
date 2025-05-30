3480df1f5f06f0bc0d8ea74ec9533efea4076646
obulaxmi <obulaxmi2701@gmail.com>
2025-05-30 17:01:44 +0530

    menu added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./Main/Navbar";
 import Homepage from "./Main/pages/Homepage";
 import Products from "./Main/pages/Products";
+import Menu from "./Main/pages/Menu"
 import Inventory from "./Main/pages/Inventory";
 import Po from "./Main/pages/PurchaseOrders";
 import Orders from "./Main/pages/Orders";
@@ -21,6 +22,7 @@ function App() {
         <Route path="/homepage" element={<Homepage />} />
         <Route path="/product" element={<Products />} />
         <Route path="/inventory" element={<Inventory />} />
+        <Route path="/menu" element={<Menu />} />
         <Route path="/purchase-orders" element={<Po />} />
         <Route path="/online-orders" element={<Orders />} />
         <Route path="/users" element={<Users />} />
