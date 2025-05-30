67c77eb6d6b015f6a78ce6c97e3a57d4ab32d24e
VelmuruganMadhesh <velukavitha005@gmail.com>
2025-05-30 14:54:54 +0530

    navbar added

--- src/Main/pages/velu.jsx
+++ src/Main/pages/velu.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Velu = () => {
   return (
-    <div className='flex h-[100vh] w-[100$] justify-center items-center text-2xl'>velmurugan</div>
+    <div className='flex h-[100vh] w-[100%] justify-center items-center text-2xl'>velmuruganM</div>
   )
 }
 
